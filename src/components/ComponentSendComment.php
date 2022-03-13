4eb53bce892c8e0fc949f9b398cbfe5536413a96
DescrySama <pierrehugot35@gmail.com>
2022-03-13 18:23:10 +0100

    ''te

---
+++ src/components/ComponentSendComment.php
@@ -0,0 +1,4 @@
+<form class="d-flex flex-column" action="" method="POST">
+    <input type="text" name="comment-content" id="comment-content" placeholder="Comment...">
+    <input class="btn btn-success m-2" type="submit" value="Poster">
+</form>
